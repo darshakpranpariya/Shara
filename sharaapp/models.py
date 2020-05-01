27244f69603df8fdd53ebcc7974b086e4b32fdec
Darshak <darshak.ranpariya@gmail.com>
2020-05-01 15:00:22 +0530

    third commit

--- sharaapp/models.py
+++ sharaapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class AllUsers(models.Model):
+    username = models.CharField(max_length=50)
+    email = models.EmailField(max_length = 254)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
+
+class DeepUserDetails(models.Model):
+    userid = models.ForeignKey(AllUsers,on_delete=models.CASCADE)
+    reputation = models.PositiveIntegerField()
+    totalupvote = models.PositiveIntegerField()
+    totaldownvote = models.PositiveIntegerField()
+    totaltips = models.PositiveIntegerField()
+
+    def __str__(self):
+        return u'%s - %d' % (self.userid.username,self.reputation)
+
